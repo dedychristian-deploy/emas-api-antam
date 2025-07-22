7d807856b112a26d6b82d839a4f32d0ca00b26f6
dedychristian-deploy <67267117+dedychristian-deploy@users.noreply.github.com>
2025-07-22 23:55:25 +0700

    Update antam.js

--- api/antam.js
+++ api/antam.js
@@ -1,13 +1,3 @@
-export default async function handler(req, res) {
-  const response = await fetch("https://www.logammulia.com/id/harga-emas-hari-ini");
-  const html = await response.text();
-
-  const match = html.match(/1 gr<\/td>\s*<td[^>]*>(.*?)<\/td>/);
-
-  if (match) {
-    const harga = match[1].replace(/[^\d]/g, "");
-    res.status(200).json({ harga: parseInt(harga), sumber: "logammulia" });
-  } else {
-    res.status(500).json({ error: "Gagal ambil harga" });
-  }
+export default function handler(req, res) {
+  res.status(200).json({ status: "API aktif!" });
 }
